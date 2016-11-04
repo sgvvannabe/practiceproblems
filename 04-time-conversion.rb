6e4e9b57b474626604f50b909a19718458f81638
Charlie <sgvvannabe@gmail.com>
2016-11-04 22:51:33 +0000

    Wrote in extra test for timeconversion problem to check 0:00

--- 04-time-conversion.rb
+++ 04-time-conversion.rb
@@ -4,11 +4,17 @@
 # Difficulty: easy.
 
 def time_conversion(minutes)
+  if minutes < 60
+    return "0:#{minutes}"
+  end
+  
+  end
 end
 
 # These are tests to check that your code is working. After writing
 # your solution, they should all print true.
 
+puts('time_conversion(0) == "0:00": ' + (time_conversion(0) == '0:00').to_s)
 puts('time_conversion(15) == "0:15": ' + (time_conversion(15) == '0:15').to_s)
 puts('time_conversion(150) == "2:30": ' + (time_conversion(150) == '2:30').to_s)
 puts('time_conversion(360) == "6:00": ' + (time_conversion(360) == '6:00').to_s)
